f22bb45e12763f10f9e00c159b330b1899ee72fd
张皓维 <zhanghaowei@me.local>
2021-03-30 00:08:26 +0800

    login_module

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        Home组件
+        <!-- 基于token的方式实现退出，只需要销毁本地的token。
+        这样，后续的请求就不会携带token，必须重新登录生成一个新的token才可以 -->
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login') // 跳转到登录页
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/' // 配置请求的根路径
+Vue.prototype.$http = axios // 使每个组件通过this都能访问到$http 从而发起ajax请求
 
 Vue.config.productionTip = false
 
